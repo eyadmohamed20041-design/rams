74f6ee8e271c60f6f40ad7b568df8ca31bed1a1a
eyadmohamed20041-design <eyadmohamed20041@gmail.com>
2026-07-11 23:22:20 +0300

    Update ser.py

--- ser.py
+++ ser.py
@@ -2,7 +2,7 @@ import os
 import re
 import time
 import logging
-
+#
 from fastapi import FastAPI, Form, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, Response
